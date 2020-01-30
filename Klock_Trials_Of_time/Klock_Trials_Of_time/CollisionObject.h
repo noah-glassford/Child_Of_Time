c9ab9b181ff166024cb9fe7a710030f45a7d0473
Noah Glassford <noahglassford@gmail.com>
2020-01-29 21:30:59 -0500

    added other files
    bruh

---
+++ Klock_Trials_Of_time/Klock_Trials_Of_time/CollisionObject.h
@@ -0,0 +1,17 @@
+#pragma once
+/*
+This is the main collision system used to organize our collision triggers
+This is not the best way to make this happen, however Box2D was being problematic with sensors.
+The way this works is by checking the collision list
+Each object of this class can be used for
+*/
+
+
+class CollisionObject
+{
+public:
+
+
+private:
+	
+};
