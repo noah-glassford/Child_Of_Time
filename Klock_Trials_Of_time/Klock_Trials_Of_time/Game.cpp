6018d1da678fbfdb67f08561cbe8e797676d49f5
Noah Glassford <noahglassford@gmail.com>
2020-04-01 12:33:44 -0400

    Combat $ menu now coexist in a happy fucking relationship

--- Klock_Trials_Of_time/Klock_Trials_Of_time/Game.cpp
+++ Klock_Trials_Of_time/Klock_Trials_Of_time/Game.cpp
@@ -49,7 +49,7 @@ void Game::InitGame()
 	//Creates a new scene.
 	//Replace this with your own scene.
 
-	m_scenes.push_back(new PhysicsTestScene("Physics Test Scene")); //0
+	m_scenes.push_back(new MainMenu("Main Menu Scene")); //0
 	m_scenes.push_back(new Level1Scene("Level 1 Scene")); //1
 	m_scenes.push_back(new Level2Scene("Level 2 Scene")); //2
 	m_scenes.push_back(new BossFightScene("Boss Fight Scene")); //3
@@ -61,7 +61,7 @@ void Game::InitGame()
 	//m_activeScene->InitScene(float(BackEnd::GetWindowWidth()), float(BackEnd::GetWindowHeight()));
 	m_activeScene->InitScene(float(BackEnd::GetWindowWidth()), float(BackEnd::GetWindowHeight()));
 
-	ECS::GetComponent<PlayerData>(1).CurrentScene = 1;
+	ECS::GetComponent<PlayerData>(1).CurrentScene = 0;
 
 	//Sets m_register to point to the register in the active scene
 	m_register = m_activeScene->GetScene();
@@ -188,8 +188,42 @@ void Game::GUI()
 	UI::End();
 }
 
+void Game::Switchscene(int scene)
+{
+	m_activeScene = m_scenes[scene];
+
+	m_activeScene->InitScene(float(BackEnd::GetWindowWidth()), float(BackEnd::GetWindowHeight()));
+
+	m_register = m_activeScene->GetScene();
+}
+
+void Game::MainMenuControlls(SDL_MouseButtonEvent event)
+{
+
+	float windowWidth = BackEnd::GetWindowWidth();
+	float windowHeight = BackEnd::GetWindowHeight();
+
+
+	if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT))
+	{
+		vec3(click) = vec3((event.x / windowWidth) * 100, (event.y / windowHeight) * 100, 0.f);
+		std::cout << click.x << "\n" << click.y << "\n";
+
+		if (click.x < 57 && click.x > 52 && click.y > 45 && click.y < 54)
+		{
+			Switchscene(1);
+		}
+	}
+}
+
+
+
 void Game::CheckEvents()
 {
+	
+	if (ECS::GetComponent<PlayerData>(1).CurrentScene == 0)
+		MainMenuControlls(BackEnd::GetClickEvent());
+	
 	if (m_close)
 		m_window->Close();
 
@@ -444,6 +478,7 @@ void Game::MouseClick(SDL_MouseButtonEvent evnt)
 {
 	//Active scene now captures this input and can use it
 	//Look at base Scene class for more info.
+	
 	m_activeScene->MouseClick(evnt);
 
 	if (m_guiActive)
